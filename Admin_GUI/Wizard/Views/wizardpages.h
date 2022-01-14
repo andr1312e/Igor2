081fdef8baeb82babdaf33dc1063a42fc1652f10
andrew <andr1312e@outlook.de>
2022-01-14 18:34:50 +0300

    2.1.4

--- Admin_GUI/Wizard/Views/wizardpages.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADMIN_GUI_VIEWS_WIZARDPAGES_H
-#define ADMIN_GUI_VIEWS_WIZARDPAGES_H
-
-enum {Page_Intro, Page_UserData, Page_FirstRole, Page_SecondRole, Page_ThirdRole, Page_FourthRole, Page_Conclusion};
-
-
-#endif // ADMIN_GUI_VIEWS_WIZARDPAGES_H
